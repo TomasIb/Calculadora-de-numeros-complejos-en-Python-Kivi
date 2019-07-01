e806db78affbea543a83e78e15590e5c2de7c4b0
TomasIb <tomas.ib94@hotmail.com>
2019-07-01 05:22:40 -0400

    bugs-fixed-point-+

--- complex_calculator.py
+++ complex_calculator.py
@@ -1,42 +1,39 @@
 import numpy as np #IMPORTACION DE LIBRERIA PARA ARREGLOS
 class ComplexCalculator():
     def __init__(self):
-        self._expression = None
-        self._tokens = []
-        self._current = None
-        self.caracters = ['+','-','*','/','(',')','[',']']
-        self.ready_expression = []
-        self.result = None
+        self._expression = None #GUARDA EXPRESION INGRESADA 
+        self._tokens = [] #GUARDA CARACTERES
+        self._current = None #VALOR ACTUAL AL RECORRER
+        self.caracters = ['+','-','*','/','(',')','[',']'] #CARACTERES ACEPTADOS
+        self.ready_expression = [] #EXPRESION SETEADA PARA SER OPERADA
+        self.result = None #RESULTADO
         
-
     def set_expression(self,parameter):
-        if(parameter != ''):
-            self._expression = parameter
+        if(parameter != ''):  #SI CARACTERES INGRESADOS SON VACIOS
+            self._expression = parameter 
         else:
-            self.expression = None
-        if(self.separate_chain()):
+            self.expression = None 
+
+        if(self.separate_chain()): #SE PUEDE SEPARAR LA CADENA?
             return True
         else:
             return False
         
-    def get_expression(self):
-        return self._expression
 
     def is_balanced_parentheses(self):
-        if(self._expression != None):
-            open_parentheses=[]
-            closed_parentheses=[]
+        if(self._expression != None): 
+            open_parentheses=[] #ARREGLO DE PARENTESIS ABRE
+            closed_parentheses=[] #ARREGLO DE PARENTESIS CIERRA
             for char in self._expression: # VERIFICA SI ESTÁ BALANCEADO
-                if(char == '[' or char == '('):
+                if(char == '[' or char == '('): # SI SE ENCUENTRA UN PARENTESIS ABRE SE GUARDA EN EL ARREGLO
                     open_parentheses.append(char)
-                elif(char == ']' or char == ')'):
+                elif(char == ']' or char == ')'): # SI SE ENCUENTRA UN PARENTESIS CIERRA SE GUARDA EN EL ARREGLO
                     closed_parentheses.append(char)
-            if(len(open_parentheses) == len(closed_parentheses)):
+            if(len(open_parentheses) == len(closed_parentheses)): #SI LOS TAMAÑOS SON IGUALES ESTAN BALANCEADOS
                 return True
             else:
                 return False
         else:
-            print('Error','Cadena Vacia') #Generar alerta
             return False
 
     def exp(self):
@@ -72,16 +69,36 @@ class ComplexCalculator():
                 self.result /= self.term()
         return self.result
 
-    def join_complex(self):
+    def create_complex(self):
         argument = self._tokens
         real = None
         imaginary = None
         negative = ['-']
+        point = ['.']
         point_negative = ['-','.']
         for i in range(len(argument)):
 
             if(argument[i] is ','):
-               
+                if any(ele in argument[i-1] for ele in point):
+                    for a in argument[i-1]:
+                        last = a
+                    if last.isdigit():
+                        continue
+                    else:
+                        return False
+                    
+                 
+    
+                elif any(ele in argument[i+1] for ele in point):
+                    for a in argument[i+1]:
+                       last = a
+                    if last.isdigit():
+                        continue
+                    else:
+                        return False
+                    
+                    
+
                 if(any(ele in argument[i-1] for ele in point_negative) and i > 0):
                     real = float(argument[i-1])
                     try_character_index = i-2; 
@@ -125,59 +142,63 @@ class ComplexCalculator():
 
 
     def separate_chain(self):
-        for i in range(len(self._expression)):
-            if (self._expression[i].isdigit() and i > 0 and  (self._tokens[-1].isdigit() or self._tokens[-1][-1] is '.')):
-                self._tokens[-1] += self._expression[i]
+        for i in range(len(self._expression)): #SE RECORRE LA EXPRESION EN BUSCA DE LAS COMBINACIONES POSIBLES
+            if (self._expression[i].isdigit() and i > 0 and  (self._tokens[-1].isdigit() or self._tokens[-1][-1] is '.')): #SI ANTES DE UN DIGITO VIENE UN DIGITO O UN PUNTO
+                self._tokens[-1] += self._expression[i] #SE CONCATENA
                 
-            elif self._expression[i] is '.' and i > 0 and self._expression[i-1].isdigit():
+            elif self._expression[i] is '.' and i > 0 and self._expression[i-1].isdigit(): #DESPUES DE UN DIGITO HAY UN PUNTO
                 self._tokens[-1] += self._expression[i]
             
-            elif self._expression[i].isdigit() and i > 0 and (self._tokens[-1][-1].isdigit()):
+            elif self._expression[i].isdigit() and i > 0 and (self._tokens[-1][-1].isdigit()):  #DESPUES DE UN DIGITO UN DIGITO
                 self._tokens[-1] += self._expression[i]  
 
-            elif self._expression[i-1] == '-' and i > 0 and self._expression[i] not in self.caracters:    
-                self._tokens[-1]+= self._expression[i]
-            
-        
+            elif self._expression[i-1] == '-' and i > 0 and self._expression[i] not in self.caracters:    #HAY UN MENOS ATRAS DEL ACTUAL Y NO ES UN CARACTER
+                self._tokens[-1]+= self._expression[i] 
 
             else:
                 self._tokens.append(self._expression[i])
+
+        
         negative = ['-']
-        for i in range(len(self._tokens)):
+        
+        for i in range(len(self._tokens)): #INSERTA UN + SI ES QUE HAY UN NEGATIVO ANTES , EL NEGATIVO SOLO ES
             
             if(any(ele in self._tokens[i] for ele in negative)):
                 index_inf = i-1
-                
-                if(index_inf > 0):
+                index_sup = i+1
+                if(index_inf > 0 and index_sup < len(self._tokens)):
                     if(self._tokens[index_inf] in self.caracters or self._tokens[index_inf] is ',' ):
                         continue
+                   
+                    if(self._tokens[index_sup] is '+'):
+                        self._tokens = self._tokens[:i] + self._tokens[index_sup:]
+                        self._tokens[index_sup] = '-'+self._tokens[index_sup]
+                        break
                     else:
-                        self._tokens.insert(i,'+')
+                        self._tokens.insert(i,'+') 
+
             
-        print(self._tokens)
+        
         return True
       
    
     def set_current(self):
-        self._current = self._tokens[0]
+        self._current = self._tokens[0] #SETEA EL PRIMER ELEMENTO
         
-        
-       
     def next(self): 
-        self._tokens = self._tokens[1:]
-        self._current = self._tokens[0] if len(self._tokens) > 0 else None
+        self._tokens = self._tokens[1:] #SACA EL ULTIMO ELEMENTO DE LA PILA
+        self._current = self._tokens[0] if len(self._tokens) > 0 else None # OBTIENE EL PRIMER ELEMENTO
 
 
     def calculate(self,parameter):
         try:
-            if(self.set_expression(parameter)):
-                if(self.is_balanced_parentheses()):
-                    self.search_tokens = self._tokens
-                    self.join_complex()
-                    self.set_current() #SETEA EL CARACTER QUE SE ESTÁ LEYENDO
-                    self.result = self.exp()
+            if(self.set_expression(parameter)): #SI SE PUEDE SETEAR LA EXPRESION INGRESADA
+                if(self.is_balanced_parentheses()): #Y LOS PARENTESIS ESTAN BALANCEADOS
+                    self.create_complex() #CREA NUMEROS COMPLEJOS
+                    self.set_current() #SETEA EL CARACTER QUE SE ESTÁ LEYENDO EN LA OPERACION
+                    self.result = self.exp() #OPERA LA CADENA
                     print ("result:",self.result)
-                    if(self.result != None):
+                    if(self.result != None): #SI EXISTE RESULTADO
                         return self.result,self.ready_expression
                     else:
                         return False,False
